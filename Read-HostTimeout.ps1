76bb7eab6395dec2e9ff2b9424592e8e77e68f3f
forevanyeung <forevanyeung@users.noreply.github.com>
2020-12-14 20:58:48 -0500

    Improved backspace and escape handling
    Accept null entry, better backspace handling, and added escape handling

--- Read-HostTimeout.ps1
+++ Read-HostTimeout.ps1
@@ -1,4 +1,4 @@
-Function read-HostTimeout {
+Function Read-HostTimeout {
 ###################################################################
 ##  Description:  Mimics the built-in "read-host" cmdlet but adds an expiration timer for
 ##  receiving the input.  Does not support -assecurestring
@@ -7,19 +7,22 @@ Function read-HostTimeout {
 ##  credit is maintained.
 ##
 ##  Written by: thegeek@thecuriousgeek.org
-##  Date Modified:  10-24-14
+##  Original Date:  10-24-14
+##  
+##  Accept null entry, better backspace handling, and escape handling
+##  Modified by: evan@forevanyeung.com
+##  Date Modified:  12-14-20
 ###################################################################
 
-# Set parameters.  Keeping the prompt mandatory
-# just like the original
+# Set parameters.
 param(
-    [Parameter(Mandatory=$true,Position=1)]
+    [Parameter(Mandatory=$false,Position=1)]
     [string]$prompt,
-
+    
     [Parameter(Mandatory=$false,Position=2)]
     [int]$delayInSeconds
 )
-
+    
     # Do the math to convert the delay given into milliseconds
     # and divide by the sleep value so that the correct delay
     # timer value can be set
@@ -27,8 +30,10 @@ param(
     $delay = ($delayInSeconds*1000)/$sleep
     $count = 0
     $charArray = New-Object System.Collections.ArrayList
-    Write-host -nonewline "$($prompt):  "
-
+    If($prompt) {
+        Write-host -nonewline "$($prompt):  "
+    }
+    
     # While loop waits for the first key to be pressed for input and
     # then exits.  If the timer expires it returns null
     While ( (!$host.ui.rawui.KeyAvailable) -and ($count -lt $delay) ){
@@ -36,36 +41,51 @@ param(
         $count++
         If ($count -eq $delay) { "`n"; return $null}
     }
-
-    # Retrieve the key pressed, add it to the char array that is storing
-    # all keys pressed and then write it to the same line as the prompt
-    $key = $host.ui.rawui.readkey("NoEcho,IncludeKeyUp").Character
-    $charArray.Add($key) | out-null
-    Write-host -nonewline $key
-
+    
     # This block is where the script keeps reading for a key.  Every time
     # a key is pressed, it checks if it's a carriage return.  If so, it exits the
     # loop and returns the string.  If not it stores the key pressed and
     # then checks if it's a backspace and does the necessary cursor 
     # moving and blanking out of the backspaced character, then resumes 
-    # writing. 
+    # writing.
+
+    $beginCursorPos = $host.ui.rawui.get_cursorPosition()
+
     $key = $host.ui.rawui.readkey("NoEcho,IncludeKeyUp")
     While ($key.virtualKeyCode -ne 13) {
         If ($key.virtualKeycode -eq 8) {
+            # Backspace handling
+            If ($host.ui.rawui.get_cursorPosition().X -gt $beginCursorPos.X) {
+                $charArray.Add($key.Character) | out-null
+                Write-host -nonewline $key.Character
+                $cursor = $host.ui.rawui.get_cursorPosition()
+                write-host -nonewline " "
+                $host.ui.rawui.set_cursorPosition($cursor)
+            }
+        } ElseIf ($key.VirtualKeyCode -eq 27) {
+            # Escape handling
+            $count = $host.ui.rawui.get_cursorPosition().X - $beginCursorPos.X
+            $host.ui.rawui.set_cursorPosition($beginCursorPos)
+            1..$count | Foreach-Object { Write-Host -NoNewline " " }
+            $host.ui.rawui.set_cursorPosition($beginCursorPos)
+            $charArray.Clear()
+        } Else {
+            # Regular character handling
             $charArray.Add($key.Character) | out-null
             Write-host -nonewline $key.Character
-            $cursor = $host.ui.rawui.get_cursorPosition()
-            write-host -nonewline " "
-            $host.ui.rawui.set_cursorPosition($cursor)
-            $key = $host.ui.rawui.readkey("NoEcho,IncludeKeyUp")
         }
-        Else {
-            $charArray.Add($key.Character) | out-null
-            Write-host -nonewline $key.Character
-            $key = $host.ui.rawui.readkey("NoEcho,IncludeKeyUp")
-        }
+        $key = $host.ui.rawui.readkey("NoEcho,IncludeKeyUp")
     }
-    ""
+    Write-Host ""
+
+    # Find the index of backspace char and pop the item to the left 
+    # of it and the backspace char item. Repeat until no more backspace chars
+    # exists
+    while(($bpos = $charArray.IndexOf([char]"`b")) -ge 0) {
+        $charArray.RemoveAt($bpos)
+        $charArray.RemoveAt($bpos-1)
+    }
+
     $finalString = -join $charArray
     return $finalString
 }
